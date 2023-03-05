98e96d9f1085d56c081bcb266c18b69b8e93b74c
AldarBk <aldarrazer20@yandex.ru>
2023-03-05 17:29:16 +0600

    in progress

---
+++ app/src/main/java/com/example/movieapp/MainActivity.kt
@@ -0,0 +1,72 @@
+package com.example.movieapp
+
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var database: DatabaseReference
+
+
+    private val signInLauncher = registerForActivityResult(
+        FirebaseAuthUIActivityResultContract()
+    ) { res ->
+        this.onSignInResult(res)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        database = Firebase.database.reference
+
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build()
+        )
+
+// Create and launch sign-in intent
+        val signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .build()
+        signInLauncher.launch(signInIntent)
+    }
+
+
+    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
+        result.idpResponse
+        if (result.resultCode == RESULT_OK) {
+            // Successfully signed in
+            val authUser = FirebaseAuth.getInstance().currentUser
+            authUser?.let {
+                val email = it.email.toString()
+                val uid = it.uid
+                val firebaseUser = User(email, uid)
+                database.child("users").child(uid).setValue(firebaseUser)
+
+
+                val intent = Intent(this, MoviesActivity::class.java)
+                startActivity(intent)
+                //
+            }
+            // ...
+        } else {
+            Toast.makeText(this@MainActivity, "Something wrong with registration", Toast.LENGTH_SHORT).show()
+            // Sign in failed. If response is null the user canceled the
+            // sign-in flow using the back button. Otherwise check
+            // response.getError().getErrorCode() and handle the error.
+            // ...
+        }
+    }
+
+
+}
